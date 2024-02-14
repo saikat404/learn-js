ca379808dae78613abd34a4b2b8b0baca2796f70
saikat404 <136243207+saikat404@users.noreply.github.com>
2024-02-14 16:58:57 +0000

    variables

--- basics/basic_1.js
+++ basics/basic_1.js
@@ -1,2 +1,6 @@
-console.log("hello world");
+var first_name = "saikat";
+let last_name ="nag";
+const emp_id = 2344;
+address = "xyz point near --- point";
 
+console.table([first_name,last_name,emp_id,address]);
